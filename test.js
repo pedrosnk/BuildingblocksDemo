4e6e724b298e3f0513f2544546d69a4044e00a04
Pedro Medeiros <pedrosnk@gmail.com>
2014-12-08 02:08:10 -0200

    deploy to heroku

--- test.js
+++ test.js
@@ -6,14 +6,40 @@ describe('Requests to Root Path', function(){
   it('returns 200 status code', function(done){
     request(app)
       .get('/')
-      .expect(200)
-      .end(function(err){
-        if (err) { throw err; }
-        done();
-      });
+      .expect(200, done);
   });
 
+  it('Returns HTML format', function(done){
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/, done);
+  });
 
+  it('Returns an index file with Cities', function(done){
+    request(app)
+      .get('/')
+      .expect(/cities/i, done);
+  });
 
 });
 
+describe('Listing cities on /cities', function(){
+
+  it('Returns 200 status code', function(done){
+    request(app)
+      .get('/cities')
+      .expect(200, done);
+  });
+
+  it('Returns JSON format', function(done){
+    request(app)
+      .get('/cities')
+      .expect('Content-Type', /json/, done)
+  });
+
+  it('Returns initial cities', function(done){
+    request(app)
+      .get('/cities')
+      .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+  });
+});
